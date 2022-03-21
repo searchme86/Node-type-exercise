eb43244923b7e086ab6bf3b9eaf200623b86d453
searchme86 <searchme86@gmail.com>
2022-03-21 23:22:15 +0900

    ✅ test controllers

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,8 @@
 export const trending = (req, res) => {
   res.send('Home Page Videos');
 };
-export const watch = (req, res) => res.send('watch');
+export const see = (req, res) => res.send('see');
 export const edit = (req, res) => res.send('edit');
+export const search = (req, res) => res.send('search');
+export const deleteVideo = (req, res) => res.send('deleteVideo');
+export const upload = (req, res) => res.send('upload');
