633650b6ca7d21b9de1a14e1baff442b0fee85f4
searchme86 <searchme86@gmail.com>
2022-03-21 23:43:24 +0900

    🎨 added routers in serverjs

--- src/server.js
+++ src/server.js
@@ -1,35 +1,16 @@
 import express from 'express';
 import logger from 'morgan';
 
+import globalRouter from './routers/globalRouter';
+import videoRouter from './routers/videoRouter';
+import userRouter from './routers/userRouter';
+
 const port = 4000;
 
 const app = express();
 const loggerMiddleware = logger('dev');
 app.use(loggerMiddleware);
-
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => {
-  res.send('Home');
-};
-
-globalRouter.get('/', handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => {
-  res.send('Edit User');
-};
-
-userRouter.get('/edit', handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => {
-  res.send('watch Videos');
-};
-
-videoRouter.get('/watch', handleWatchVideo);
+app.set('view engine', 'pug');
 
 app.use('/', globalRouter);
 app.use('/videos', videoRouter);
