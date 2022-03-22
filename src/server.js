260d51b0ce4bb83f734905916e07eb64924da3c8
searchme86 <searchme86@gmail.com>
2022-03-22 12:48:46 +0900

    ⚡️ added settigns for view, form

--- src/server.js
+++ src/server.js
@@ -6,11 +6,14 @@ import videoRouter from './routers/videoRouter';
 import userRouter from './routers/userRouter';
 
 const port = 4000;
-
 const app = express();
 const loggerMiddleware = logger('dev');
+
 app.use(loggerMiddleware);
 app.set('view engine', 'pug');
+app.set('views', process.cwd() + '/src/views');
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', globalRouter);
 app.use('/videos', videoRouter);
