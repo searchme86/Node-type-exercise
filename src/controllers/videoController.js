726ee836f5c3395f63c7ab889d8234e03a927fe7
searchme86 <searchme86@gmail.com>
2022-03-22 19:04:52 +0900

    🎨  refactored for form POST action

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,71 @@
+import res from 'express/lib/response';
+
+let videos = [
+  {
+    title: 'First Video',
+    rating: 5,
+    comments: 2,
+    createdAt: '2 minutes ago',
+    views: 1,
+    id: 1,
+  },
+  {
+    title: 'Second Video',
+    rating: 5,
+    comments: 2,
+    createdAt: '2 minutes ago',
+    views: 59,
+    id: 2,
+  },
+  {
+    title: 'Third Video',
+    rating: 5,
+    comments: 2,
+    createdAt: '2 minutes ago',
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  res.send('Home Page Videos');
+  return res.render('home', { pageTitle: 'Home', videos });
 };
-export const see = (req, res) => res.send('see');
-export const edit = (req, res) => res.send('edit');
-export const search = (req, res) => res.send('search');
-export const deleteVideo = (req, res) => res.send('deleteVideo');
-export const upload = (req, res) => res.send('upload');
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('watch', {
+    pageTitle: `Watching : ${video.title}`,
+    video,
+  });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('edit', { pageTitle: `Editing: ${video.title}`, video });
+};
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (re, res) => {
+  return res.render('upload', { pageTitle: 'Upload Video' });
+};
+
+export const postUpload = (req, res) => {
+  console.log(req.body);
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 5,
+    comments: 2,
+    createdAt: 'just now',
+    views: 59,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect('/');
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -6,11 +6,15 @@ import {
   upload,
   getEdit,
   postEdit,
+  getUpload,
+  postUpload,
 } from '../controllers/videoController';
 
 const videoRouter = express.Router();
 
 videoRouter.route('/:id(\\d+)').get(watch);
 videoRouter.route('/:id(\\d+)/edit').get(getEdit).post(postEdit);
+
+videoRouter.route('/upload').get(getUpload).post(postUpload);
 
 export default videoRouter;
