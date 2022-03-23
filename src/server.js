0e99438a546be2e6d0ee71556fcd82be992f4215
searchme86 <searchme86@gmail.com>
2022-03-23 20:51:19 +0900

    🚚  renamed: imported file renamed

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 
-import globalRouter from './routers/globalRouter';
+import rootRouter from './routers/rootRouter';
 import videoRouter from './routers/videoRouter';
 import userRouter from './routers/userRouter';
 
@@ -14,7 +14,7 @@ app.set('views', process.cwd() + '/src/views');
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', globalRouter);
+app.use('/', rootRouter);
 app.use('/videos', videoRouter);
 app.use('/users', userRouter);
 
