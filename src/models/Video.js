d2c3aecf774cdac784e3e034babf00359fba553c
searchme86 <searchme86@gmail.com>
2022-03-23 15:59:52 +0900

    ⚡️ introduced static of mongoose function

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,14 +1,20 @@
 import mongoose from 'mongoose';
 
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  createdAt: { type: Date, required: true },
-  hashtags: [{ type: String }],
+  title: { type: String, required: true, trim: true, maxlength: 80 },
+  description: { type: String, required: true, trim: true, minlength: 20 },
+  createdAt: { type: Date, required: true, default: Date.now },
+  hashtags: [{ type: String, trim: true }],
   meta: {
-    views: Number,
-    rating: Number,
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
   },
+});
+
+videoSchema.static('formatHashtags', function (hashtags) {
+  return hashtags
+    .split(',')
+    .map((word) => (word.startsWith('#') ? word : `#${word}`));
 });
 
 const Video = mongoose.model('Video', videoSchema);
