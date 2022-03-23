46455a44c28471b93f3313d0ad7b4a09121da51c
searchme86 <searchme86@gmail.com>
2022-03-23 17:39:52 +0900

    ⚡️ added: a search router

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { join, login } from '../controllers/userController';
-import { home } from '../controllers/videoController';
+import { home, search } from '../controllers/videoController';
 
 const globalRouter = express.Router();
 
 globalRouter.route('/').get(home);
 globalRouter.route('/join').get(join);
 globalRouter.route('/login').get(login);
+globalRouter.route('/search').get(search);
 
 export default globalRouter;
